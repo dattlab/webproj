68353d84a48739887f45bf4b5cc0363aa4a16289
Datsudo <76833632+DatSudo@users.noreply.github.com>
2023-03-31 02:02:36 +0800

    Add calculator app

---
+++ calculator.js
@@ -0,0 +1,23 @@
+// Load plugins
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+})
+
+app.post("/", (req, res) => {
+  let postData = req.body;
+
+  res.send(`Sum = ${parseFloat(postData.num1) + parseFloat(postData.num2)}`);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
