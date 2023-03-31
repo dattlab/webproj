94ed9cc6f3bcf16c73d75a40da9ade9039200a63
Datsudo <76833632+DatSudo@users.noreply.github.com>
2023-03-31 18:39:09 +0800

    Add https module

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // Load plugins
 const express = require("express");
+const https = require("https");
+
+// Create express instance
 const app = express();
 const port = 3000;
+
+const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+const appID = "9ecf957ed3df3456f632767e5b0f93d3";
 
 // Use express body parser ---------------------------
 app.use(express.urlencoded({extended: true}));
@@ -12,6 +18,15 @@ app.use(express.static(`${__dirname}/public`));
 
 // Get-Post for main page ---------------------------
 app.get("/", (req, res) => {
+  let city = "quezon+city";
+  let countryCode = "ph";
+  let units = "metric";
+  let getURL = `${apiEndpoint}?q=${city},${countryCode}&units=${units}&appid=${appID}`;
+
+  https.get(getURL, (response) => {
+    console.log(response)
+  })
+
   res.sendFile(`${__dirname}/index.html`);
 })
 
