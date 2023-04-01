01f01f5b31ec6fc8d2fca092c4d590141c178230
Datsudo <76833632+DatSudo@users.noreply.github.com>
2023-04-01 10:59:00 +0800

    Update weather app

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 const port = 3000;
 
 const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-const appID = "9ecf957ed3df3456f632767e5b0f93d3";
+const iconURLBase = "https://openweathermap.org/img/wn"
+const apiKey = "9ecf957ed3df3456f632767e5b0f93d3";
 
 // Use express body parser ---------------------------
 app.use(express.urlencoded({extended: true}));
@@ -18,21 +19,69 @@ app.use(express.static(`${__dirname}/public`));
 
 // Get-Post for main page ---------------------------
 app.get("/", (req, res) => {
-  let city = "quezon+city";
-  let countryCode = "ph";
+  res.sendFile(`${__dirname}/index.html`);
+
+})
+app.post("/index.html", (req, res) => {
+  
+  let city = capitalize(req.body.cityName);
   let units = "metric";
-  let getURL = `${apiEndpoint}?q=${city},${countryCode}&units=${units}&appid=${appID}`;
+  let getURL = `${apiEndpoint}?q=${city.replace(/ /g, "+")}&units=${units}&appid=${apiKey}`;
 
   https.get(getURL, (response) => {
-    console.log(response)
-  })
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
 
-  res.sendFile(`${__dirname}/index.html`);
-})
+      const countryCode = weatherData.sys.country;
+      const cityTemp = weatherData.main.temp;
+      const weatherDesc = weatherData.weather[0].description;
+      const windDirection = getWindDirection(weatherData.wind.deg);
+      const icon = weatherData.weather[0].icon;
+      const iconURL = `${iconURLBase}/${icon}@2x.png`;
 
+      res.send(`
+        <hr>
+        <div class="weather-card">
+          <img src="${iconURL}" alt="weather icon" name="weather icon">
+          <h1>${city} <span style="font-size: 1.2rem">${getFlagEmoji(countryCode)} ${countryCode}</span></h1>
+          <p><strong>Temperature (C):</strong> ${cityTemp}</p>
+          <p><strong>Description:</strong> ${weatherDesc}</p>
+          <p><strong>Wind direction:</strong> ${windDirection}</p>
+        </div>
+        <hr>
+      `);
+    });
+  });
+
+});
 
 // Port listener ---------------------------
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
+
+
+// Functions ---------------------------
+function capitalize(str) {
+  const words = str.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+  }
+
+  return words.join(" ");
+}
+
+function getWindDirection(angle) {
+  const directions = ['↑ N', '↗ NE', '→ E', '↘ SE', '↓ S', '↙ SW', '← W', '↖ NW'];
+  return directions[Math.round(angle / 45) % 8];
+}
+
+function getFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char =>  127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
 
