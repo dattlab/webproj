afed0e1674855b52f96e56c7b914a50ea74e476a
datsudo <76833632+DatSudo@users.noreply.github.com>
2023-04-11 17:41:13 +0800

    ToDo list v1

--- app.js
+++ app.js
@@ -9,20 +9,47 @@ const port = 3000;
 // ----- Use express body parser
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 // ----- Load public files
 app.use(express.static(`${__dirname}/public`));
 
+// ----- Set EJS
+app.set("view engine", "ejs");
+
+let taskList = [];
 
 // ----- Get-Post for main page
-app.get("/", (_, res) => {
+app.get("/", (req, res) => {
 
-  res.sendFile(`${__dirname}/index.html`);
+  let today = new Date(); 
+  let dateOpts = {
+    weekday: "long",
+    day: "numeric",
+    month: "short"
+  }
+
+  let dayReadable = today.toLocaleString('en-US', dateOpts);
+
+  let isWeekend = (today.getDay() == 6 || today.getDay() == 0) ? true : false;
+  let status = (isWeekend) ? "Time to party!" : "Time to work.";
+
+  res.render("list", {
+    dayReadable: dayReadable,
+    isWeekend: isWeekend,
+    status: status,
+    taskList: taskList
+  });
+
 
 })
 
-app.post("/", (_, res) => {
+app.post("/", (req, res) => {
+  let newTask = req.body.newTask;
 
-  res.send();
+  if (newTask !== "" && !(count(taskList, newTask) === 1))
+    taskList.push(req.body.newTask);
+
+  res.redirect("/");
 
 });
 
@@ -31,3 +58,8 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
 
+// ----- Functions
+function count(arr, element) {
+  /* Count number of specified element in array */
+  return arr.filter(e => e === element).length;
+}
