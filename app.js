cde009050b77ed7bd1aec200e73eafeea4d1c9cb
dattlab <122553391+dattlab@users.noreply.github.com>
2023-04-12 17:41:41 +0800

    Add starting files for blog project

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // ----- Load plugins
 const express = require("express");
 const https = require("https");
-const date = require(`${__dirname}/date.js`);
 
 // ----- Create express instance
 const app = express();
@@ -18,49 +17,15 @@ app.use(express.static(`${__dirname}/public`));
 app.set("view engine", "ejs");
 
 // ----- Global variables
-const personalTasks = [];
-const workTasks = [];
 
 // ----- Get-Post for main page
 app.get("/", (req, res) => {
-  let categ = "personal"
-
-  res.render("list", {
-    dayReadable: date.getDateInfo().dayReadable,
-    isWeekend: date.getDateInfo().isWeekend,
-    categ: categ,
-    taskList: personalTasks
-  });
 })
 
 app.post("/", (req, res) => {
-  let newTask = req.body.newTask;
-  let redirectPage = "/";
-  let temp = req.body.list === "personal" ? personalTasks : workTasks;
-
-  if (newTask !== "" && !(temp.filter(e => e === newTask).length === 1)) {
-    if (req.body.list === "personal") {
-      personalTasks.push(req.body.newTask);
-    }
-    else {
-      workTasks.push(req.body.newTask);
-      redirectPage = "/work";
-    }
-  }
-
-  res.redirect(redirectPage);
 });
 
-app.get("/work", (req, res) => {
-  let categ = "work";
 
-  res.render("list", {
-    dayReadable: date.dayReadable,
-    isWeekend: date.isWeekend,
-    categ: categ,
-    taskList: workTasks
-  });
-})
 
 // ----- Port listener
 app.listen(port, () => {
